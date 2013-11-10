e3c26258bcdca1b03049d9a69ed6a86448c5b055
Eugene Wilson <etwilson@gmail.com>
2013-11-10 17:25:20 -0500

    Added authentication to site

--- awcc_website/urls.py
+++ awcc_website/urls.py
@@ -1,18 +1,12 @@
 from django.conf.urls import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'awcc_website.views.home', name='home'),
-    # url(r'^awcc_website/', include('awcc_website.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
+    url(r'^auth/', 'customer_report.views.auth_and_login'),
+    url(r'^login/', 'customer_report.views.loginview'),
+    url(r'^logout/', 'customer_report.views.logout_view'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^grouped_time/$', 'customer_report.views.grouped_time'), 
     url(r'^$', 'awcc_website.views.index'), 
--- customer_report/views.py
+++ customer_report/views.py
@@ -1,7 +1,10 @@
-from django.template import Context, loader, RequestContext
-from django.shortcuts import render_to_response
-from django.http import HttpResponse
+from django.core.context_processors import csrf
+from django.contrib.auth.decorators import login_required
+from django.template import RequestContext
+from django.shortcuts import render_to_response, redirect
 import django_tables2 as tables
+from django.contrib.auth import authenticate, login, logout
+
 from customer_report.models import Order
 import customer_report.ecwid
 
@@ -9,6 +12,27 @@ class OrderTable(tables.Table):
     class Meta:
         model = Order
 
+def auth_and_login(request, onsuccess='/', onfail='/login/'):
+    user = authenticate(username=request.POST['username'], 
+            password=request.POST['password'])
+    if user is not None:
+        login(request, user)
+        print "Login success"
+        return redirect(onsuccess)
+    else:
+        print "Login failed"
+        return redirect(onfail)  
+
+def loginview(request):
+    context = {}
+    context.update(csrf(request))
+    return render_to_response('login.html', context)
+
+def logout_view(request):
+    logout(request)
+    return redirect("/login/")
+
+@login_required(login_url='/login/')
 def grouped_time(request):
     customer_report.ecwid.refresh_order_data()
 
